4cebbd01fddbb237fd1abe485df98ed8f7e80514
George F <78384472+Wozunubi@users.noreply.github.com>
2022-11-30 18:06:29 -0500

    Add files via upload

---
+++ 2021/script.js
@@ -0,0 +1,17 @@
+function changeToDonkey(){
+    document.getElementById("temp").src = "/Donkey_in_Clovelly,_North_Devon,_England.jpg";
+
+    document.getElementById("here").style.color = "blue";
+
+    document.getElementById("here").innerHTML = "Donkey!";
+}
+
+function changeToFarm(){
+    document.getElementById("temp").src = "/Farm.jpg";
+}
+
+/*function changeName() {
+    console.log("Name again");
+}*/
+
+//console.log("test");  `
